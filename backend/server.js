83f1885acbf22200f98e06ec97ba20e955e91e68
Abbas <abbas.code786@gmail.com>
2025-08-21 17:44:46 +0530

    frontend components built

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import productRoutes from "./routes/product.route.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Routes
 app.use("/api/products", productRoutes);

3662586ef6b0eb70e49927a4e715464523211d11
Abbas <abbas.code786@gmail.com>
2025-08-21 11:55:56 +0530

    built all the API's

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
 const app = express();
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
+// Routes
+app.use("/api/products", productRoutes);
 
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all the details" });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("Error in create product controller: ", error.message);
-    res.status(500).json({ success: false, message: "Internal server error" });
-  }
-});
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("server running at the port 5000");
+  console.log("server running at the port " + PORT);
 });
